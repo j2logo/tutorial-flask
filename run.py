f7450430c375c99df79526eb024552f2ddd0cb90
j2logo <juanjo@j2logo.com>
2018-12-22 19:54:57 +0100

    Añade la página home, post_view y post_form + estilos + plantilla base

--- run.py
+++ run.py
@@ -6,10 +6,25 @@ FECHA DE CREACIÓN: 2/12/18
 
 """
 
-from flask import Flask
+from flask import Flask, render_template
+
 app = Flask(__name__)
 
 
-@app.route('/')
-def hello_world():
-    return 'Hello, World!'
+posts = []
+
+
+@app.route("/")
+def index():
+    return render_template("index.html", num_posts=len(posts))
+
+
+@app.route("/p/<string:slug>/")
+def show_post(slug):
+    return render_template("post_view.html", slug_title=slug)
+
+
+@app.route("/admin/post/")
+@app.route("/admin/post/<int:post_id>/")
+def post_form(post_id=None):
+    return render_template("admin/post_form.html", post_id=post_id)
