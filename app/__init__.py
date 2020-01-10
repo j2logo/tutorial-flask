cff33112fd7fc87e949658c92cdd35c2820c0610
j2logo <juanjo@j2logo.com>
2020-01-10 14:26:06 +0100

    Añade mensajes de log

--- app/__init__.py
+++ app/__init__.py
@@ -6,6 +6,9 @@ FECHA DE CREACIÓN: 24/05/2019
 
 """
 
+import logging
+from logging.handlers import SMTPHandler
+
 from flask import Flask, render_template
 from flask_login import LoginManager
 from flask_sqlalchemy import SQLAlchemy
@@ -23,6 +26,8 @@ def create_app(settings_module):
         app.config.from_pyfile('config-testing.py', silent=True)
     else:
         app.config.from_pyfile('config.py', silent=True)
+
+    configure_logging(app)
 
     login_manager.init_app(app)
     login_manager.login_view = "login"
@@ -54,3 +59,71 @@ def register_error_handlers(app):
     @app.errorhandler(404)
     def error_404_handler(e):
         return render_template('404.html'), 404
+
+
+def configure_logging(app):
+    """
+    Configura el módulo de logs. Establece los manejadores para cada logger.
+
+    :param app: Instancia de la aplicación Flask
+
+    """
+
+    # Elimina los manejadores por defecto de la app
+    del app.logger.handlers[:]
+
+    loggers = [app.logger, ]
+    handlers = []
+
+    console_handler = logging.StreamHandler()
+    console_handler.setFormatter(verbose_formatter())
+
+    if (app.config['APP_ENV'] == app.config['APP_ENV_LOCAL']) or (
+            app.config['APP_ENV'] == app.config['APP_ENV_TESTING']) or (
+            app.config['APP_ENV'] == app.config['APP_ENV_DEVELOPMENT']):
+        console_handler.setLevel(logging.DEBUG)
+        handlers.append(console_handler)
+    elif app.config['APP_ENV'] == app.config['APP_ENV_PRODUCTION']:
+        console_handler.setLevel(logging.INFO)
+        handlers.append(console_handler)
+
+        mail_handler = SMTPHandler((app.config['MAIL_SERVER'], app.config['MAIL_PORT']),
+                                   app.config['DONT_REPLY_FROM_EMAIL'],
+                                   app.config['ADMINS'],
+                                   '[Error][{}] La aplicación falló'.format(app.config['APP_ENV']),
+                                   (app.config['MAIL_USERNAME'],
+                                    app.config['MAIL_PASSWORD']),
+                                   ())
+        mail_handler.setLevel(logging.ERROR)
+        mail_handler.setFormatter(mail_handler_formatter())
+        handlers.append(mail_handler)
+
+    for l in loggers:
+        for handler in handlers:
+            l.addHandler(handler)
+        l.propagate = False
+        l.setLevel(logging.DEBUG)
+
+
+def mail_handler_formatter():
+    return logging.Formatter(
+        '''
+            Message type:       %(levelname)s
+            Location:           %(pathname)s:%(lineno)d
+            Module:             %(module)s
+            Function:           %(funcName)s
+            Time:               %(asctime)s.%(msecs)d
+
+            Message:
+
+            %(message)s
+        ''',
+        datefmt='%d/%m/%Y %H:%M:%S'
+    )
+
+
+def verbose_formatter():
+    return logging.Formatter(
+        '[%(asctime)s.%(msecs)d]\t %(levelname)s \t[%(name)s.%(funcName)s:%(lineno)d]\t %(message)s',
+        datefmt='%d/%m/%Y %H:%M:%S'
+    )
